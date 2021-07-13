5fc516c6aaf140f54b980751ec831a8f71ef657b
Mfurkansengul <muhammedfurkan.sengul@gmail.com>
2021-07-13 17:19:51 +0300

    Düzenleme

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
 
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, ButtonGroup, ButtonToolbar, Dropdown, DropdownButton, Carousel} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header id="header">
       <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+  <Navbar.Brand href="#">Logo</Navbar.Brand>
   <Navbar.Toggle aria-controls="navbarScroll" />
   <Navbar.Collapse id="navbarScroll">
     <Nav
@@ -15,19 +16,7 @@ function App() {
       style={{ maxHeight: '100px' }}
       navbarScroll
     >
-      <Nav.Link href="#action1">Home</Nav.Link>
-      <Nav.Link href="#action2">Link</Nav.Link>
-      <NavDropdown title="Link" id="navbarScrollingDropdown">
-        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-      </NavDropdown>
-      <Nav.Link href="#" disabled>
-        Link
-      </Nav.Link>
-    </Nav>
-    <Form className="d-flex">
+          <Form className="d-flex">
       <FormControl
         type="search"
         placeholder="Search"
@@ -36,56 +25,157 @@ function App() {
       />
       <Button variant="outline-success">Search</Button>
     </Form>
+      <Nav.Link href="#action1">Sign In</Nav.Link>
+      <NavDropdown title="Notification" id="navbarScrollingDropdown">
+      </NavDropdown>
+      <Nav.Link href="#action2">Cart</Nav.Link>
+    </Nav>
+
   </Navbar.Collapse>
 </Navbar>
       </header>
       <section id="categories">
-        <div id="categorie" className="container">
-          <div className="dropdown m-2">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown button
-            </button>
-            <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-              <li><a className="dropdown-item active" href="#">Action</a></li>
-              <li><a className="dropdown-item" href="#">Another action</a></li>
-              <li><a className="dropdown-item" href="#">Something else here</a></li>
-              <li><hr className="dropdown-divider" /></li>
-              <li><a className="dropdown-item" href="#">Separated link</a></li>
-            </ul>
-          </div>
-        </div>
-        <ul>
-          <div className="btn-toolbar" role="toolbar">
-            <div className="btn-group me-2">
-              <button className="btn btn-outline-secondary me-2" type="button"><a href="#">Offers</a></button>
-              <button className="btn btn-outline-secondary me-2" type="button"><a href="#">Top Sold</a></button>
-              <button className="btn btn-outline-secondary me-2" type="button"><a href="#">Other Products</a></button>
-              <button className="btn btn-outline-secondary me-2" type="button"><a href="#">Gift Ideas</a></button>
-            </div>
-          </div>
-          <li><a href="">Language/Currency</a></li>
-        </ul>
+      <ButtonToolbar aria-label="Toolbar with button groups">
+        <ButtonGroup className="mr-2">
+          <NavDropdown title="All Categories" id="navbarScrollingDropdown">Tüm Kategoriler</NavDropdown>
+        </ButtonGroup>
+        <ButtonGroup className="mr-2">
+          <Button>Offers</Button>
+        </ButtonGroup >
+        <ButtonGroup className="mr-2">
+          <Button>Top Sold</Button>
+        </ButtonGroup >
+        <ButtonGroup className="mr-2">
+          <Button>Other Products</Button>
+        </ButtonGroup >
+        <ButtonGroup className="mr-2">
+          <Button>Gift Ideas</Button>
+        </ButtonGroup >
+        <ButtonGroup className="mr-2">
+          <NavDropdown title="Language/Currency" id="navbarSrollingDropdown"></NavDropdown>
+        </ButtonGroup >
+        </ButtonToolbar>
       </section>
       <section id="subcategories">
-        <div>Sub Categories</div>
-        <div id="carouselExampleIntervar" className="carousel slide" data-bs-ride="carousel">
-          <div className="carousel-inner">
-            <div className="carousel-item active" data-bs-interval="5000">
-              <img src="" className="d-block w-100" alt="..." />
-            </div>
-            <div className="carousel-item" data-bs-interval="2000">
-              <img src="..." className="d-block w-100" alt="..." />
-            </div>
-          </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>          
-        </div>
+      <div className="mb-2">
+    {['right'].map((direction) => (
+      <DropdownButton
+        as={ButtonGroup}
+        key={direction}
+        id={`dropdown-button-drop-${direction}`}
+        drop={direction}
+        variant="secondary"
+        title={` A ${direction} `}
+      >
+        <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+      </DropdownButton>
+    ))}
+  </div>
+  <div className="mb-2">
+    {['right'].map((direction) => (
+      <DropdownButton
+        as={ButtonGroup}
+        key={direction}
+        id={`dropdown-button-drop-${direction}`}
+        drop={direction}
+        variant="secondary"
+        title={` B ${direction} `}
+      >
+        <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+      </DropdownButton>
+    ))}
+  </div>
+  <div className="mb-2">
+    {['right'].map((direction) => (
+      <DropdownButton
+        as={ButtonGroup}
+        key={direction}
+        id={`dropdown-button-drop-${direction}`}
+        drop={direction}
+        variant="secondary"
+        title={` C ${direction} `}
+      >
+        <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+      </DropdownButton>
+    ))}
+  </div>
+  <div className="mb-2">
+    {['right'].map((direction) => (
+      <DropdownButton
+        as={ButtonGroup}
+        key={direction}
+        id={`dropdown-button-drop-${direction}`}
+        drop={direction}
+        variant="secondary"
+        title={` D ${direction} `}
+      >
+        <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+        </DropdownButton>
+    ))}
+  </div>
+  <div className="mb-2">
+    {['right'].map((direction) => (
+      <DropdownButton
+        as={ButtonGroup}
+        key={direction}
+        id={`dropdown-button-drop-${direction}`}
+        drop={direction}
+        variant="secondary"
+        title={` E ${direction} `}
+      >
+        <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+      </DropdownButton>
+    ))}
+  </div>
+  <div className="mb-2">
+    {['right'].map((direction) => (
+      <DropdownButton
+        as={ButtonGroup}
+        key={direction}
+        id={`dropdown-button-drop-${direction}`}
+        drop={direction}
+        variant="secondary"
+        title={` F ${direction} `}
+      >
+        <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+        </DropdownButton>
+    ))}
+  </div>
+  <Carousel>
+  <Carousel.Item interval={1000}>
+    <img
+      className="d-block w-100"
+      src="images\IMG_0117.JPG"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={500}>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Second slide&bg=282c34"
+      alt="Second slide"
+    />
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Third slide&bg=20232a"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
       </section>
       <section id="clearence">
         <div>Clearence</div>
